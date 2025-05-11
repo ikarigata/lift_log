dbdf6fe54eb82a2da99193c91687971a44e07ad3
a.daitoku <0718.asuka@gmail.com>
2025-05-11 23:01:28 +0900

    spring initializerでspringbootの初期構成を作成

---
+++ backend/src/main/java/io/github/ikarigata/lift_log/LiftLogApplication.java
@@ -0,0 +1,13 @@
+package io.github.ikarigata.lift_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftLogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/ikarigata/lift_log/LiftLogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ikarigata.lift_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
